00ab5e9aa64e8062c074da1205288c5eae95de44
alvine2200 <alvinellavu@gmail.com>
2024-05-04 11:34:13 +0300

    first commit

---
+++ src/test/java/com/crudApplication/SpringCrud/SpringCrudApplicationTests.java
@@ -0,0 +1,13 @@
+package com.crudApplication.SpringCrud;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringCrudApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

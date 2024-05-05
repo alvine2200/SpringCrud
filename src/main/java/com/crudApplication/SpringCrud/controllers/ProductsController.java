445c57b36410b6b1b503ff7b6b6a9f0f4fcde74f
alvine2200 <alvinellavu@gmail.com>
2024-05-05 14:22:11 +0300

    crud complete

--- src/main/java/com/crudApplication/SpringCrud/controllers/ProductsController.java
+++ src/main/java/com/crudApplication/SpringCrud/controllers/ProductsController.java
@@ -15,6 +15,8 @@ import org.springframework.validation.FieldError;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 
+import java.io.File;
+import java.io.IOException;
 import java.io.InputStream;
 import java.nio.file.Files;
 import java.nio.file.Path;
@@ -55,10 +57,6 @@ public class ProductsController {
             bindingResult.addError(new FieldError("productRequest","imageFileName","Product must have an image"));
         }
 
-        if(bindingResult.hasErrors()){
-            return "products/create";
-        }
-
         MultipartFile image = productRequest.getImageFileName();
         Date createdAt = new Date();
         String imageName = createdAt.getTime() + "_" + image.getOriginalFilename();
@@ -78,10 +76,15 @@ public class ProductsController {
         {
             System.out.println("Exception:" + ex.getMessage());
         }
+
+        if(bindingResult.hasErrors()){
+            return "products/create";
+        }
             Product product = new Product();
             product.setName(productRequest.getName());
             product.setBrand(productRequest.getBrand());
             product.setCategory(productRequest.getCategory());
+            product.setDescription(productRequest.getDescription());
             product.setImageFileName(imageName);
             product.setPrice(productRequest.getPrice());
             product.setCreatedAt(createdAt);
@@ -92,10 +95,96 @@ public class ProductsController {
         return "redirect:/products";
     }
 
-    @GetMapping("/edit{id}")
-    public Optional<Product> editProduct(@RequestBody Model model, Long id){
-        Optional<Product> product = productRepository.findById(id);
-        model.addAttribute("product",product);
-        return product;
+    @GetMapping("/edit")
+    public String editProduct(Model model, @RequestParam Long id){
+        try {
+            Product product = productRepository.findById(id).get();
+            model.addAttribute("product", product);
+
+            ProductRequest productRequest = new ProductRequest();
+            productRequest.setName(product.getName());
+            productRequest.setBrand(product.getBrand());
+            productRequest.setCategory(product.getCategory());
+            productRequest.setPrice(product.getPrice());
+            productRequest.setDescription(product.getDescription());
+
+            model.addAttribute("productRequest",productRequest);
+
+//            product.setCreatedAt(product.getCreatedAt());
+        }catch (Exception ex) {
+            System.out.println("Exception:" + ex.getMessage());
+            return "redirect:/products";
+        }
+        return "products/edit";
+    }
+
+
+
+    @PostMapping("/edit")
+    public String updateProduct(Model model, @RequestParam Long id,@Valid @ModelAttribute ProductRequest productRequest, BindingResult bindingResult)
+    {
+        try{
+                Product product = productRepository.findById(id).get();
+                model.addAttribute("product",product);
+
+                if (bindingResult.hasErrors())
+                {
+                    return "redirect:/products/edit";
+                }
+
+                if (!productRequest.getImageFileName().isEmpty()){
+                    String uploadDir = "public/images/";
+                    Path oldImagePath = Paths.get(uploadDir + product.getImageFileName());
+
+                    try{
+                        Files.delete(oldImagePath);
+                    }catch (Exception ex) {
+                        System.out.println("Exception:" + ex.getMessage());
+                    }
+
+                    //new image after deleting the old image
+                    try {
+                        MultipartFile image = productRequest.getImageFileName();
+                        Date createdAt = new Date();
+                        String imageName = createdAt.getTime() + "_" + image.getOriginalFilename();
+
+                        String uploadDi = "public/images/";
+                        Path uploadPath = Paths.get(uploadDi);
+
+                        if (!Files.exists(uploadPath)) {
+                            Files.createDirectories(uploadPath);
+                        }
+
+                        InputStream inputStream = image.getInputStream();
+                        Files.copy(inputStream,Paths.get(uploadDir + imageName), StandardCopyOption.REPLACE_EXISTING);
+
+                        product.setImageFileName(imageName);
+                    }catch (Exception ex)
+                    {
+                        System.out.println("Exception:" + ex.getMessage());
+                    }
+                }
+                product.setName(productRequest.getName());
+                product.setBrand(productRequest.getBrand());
+                product.setCategory(productRequest.getCategory());
+                product.setPrice(productRequest.getPrice());
+                product.setDescription(productRequest.getDescription());
+                productRepository.save(product);
+        }catch (Exception ex)
+        {
+            System.out.println("Exception:" + ex.getMessage());
+            return "redirect:/products/edit";
+        }
+        return "redirect:/products";
+    }
+
+    @GetMapping("/delete")
+    public String deleteProduct(@RequestParam Long id) throws IOException {
+        Product product = productRepository.findById(id).get();
+
+        Path imagePath = Paths.get("public/images/" + product.getImageFileName());
+        Files.delete(imagePath);
+        productRepository.delete(product);
+        return "redirect:/products";
     }
 }
